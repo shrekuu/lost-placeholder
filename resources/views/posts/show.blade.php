8a74d00229890483bbee765c11c7df8476ee490b
shrekuu <shrekuu@163.com>
2019-09-24 18:36:48 +0800

    added post style

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,19 +1,22 @@
 @extends("layouts.app")
 
 @section('styles')
-    <link href="{{ mix("css/pages/posts/show.css") }}" rel="stylesheet">
+    <link href="{{ mix("/css/pages/posts/show.css") }}" rel="stylesheet">
 @endsection
 
 @section('content')
 
     <div class="post-container">
 
+        <div class="cover" style="background-image: url({{ $post->cover }});"></div>
+
         <h3 class="title">{{ $post->title }}</h3>
 
-        <div class="meta">
-            <div class="author">
-                <img class="avatar" src="{{ $post->author->avatar }}">
+        <div class="author">
+            <img class="avatar" src="{{ $post->author->avatar }}">
+            <div class="detail">
                 <div class="nickname">{{ $post->author->nickname }}</div>
+                <div class="published-at">{{ $post->author->updated_at }}</div>
             </div>
         </div>
 
