1e628380689be74081bf8845578427f17ab1277b
shrekuu <shrekuu@163.com>
2019-09-24 17:04:30 +0800

    fix posts pagination

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::select(['id', 'title', 'cover', 'user_id'])->get();
+        $posts = Post::select(['id', 'title', 'cover', 'user_id'])->paginate();
         return jsonResponse($posts);
     }
 
